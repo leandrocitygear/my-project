4ea6bd1d6a6eff201e40a5d9f8481b1bdbfb27de
Leandro De la cruz <140254019+leandrocitygear@users.noreply.github.com>
2025-10-17 12:48:59 +0000

    added symbols

--- src/components/Transactions/RecentTransactions.jsx
+++ src/components/Transactions/RecentTransactions.jsx
@@ -3,21 +3,22 @@
 function RecentTransactions () {
 
     const transactionsMockData = [
-        { date: "Oct 15, 2025", description: "Freelance Project", category: "Deposit", type: "Income", amount: "+ $500.00" },
-        { date: "Oct 12, 2025", description: "Groceries", category: "Food", type: "Expense", amount: "- $80.00" },
-        { date: "Oct 10, 2025", description: "Water bill", category: "Utility", type: "Expense", amount: "- $40.00" },
-        { date: "Oct 9, 2025", description: "Work", category: "Paycheck", type: "Income", amount: "+ $2380.00" }
-
+        { date: "Oct 15, 2025", description: "Freelance Project", category: "Deposit", type: "Income", amount: "$500.00" },
+        { date: "Oct 12, 2025", description: "Groceries", category: "Food", type: "Expense", amount: "$80.00" },
+        { date: "Oct 10, 2025", description: "Water bill", category: "Utility", type: "Expense", amount: "$40.00" },
+        { date: "Oct 9, 2025", description: "Work", category: "Paycheck", type: "Income", amount: "$2380.00" },
+        { date: "Oct 3, 2025", description: "Friend send money", category: "Venmo", type: "Income", amount: "$180.00" },
+        { date: "Oct 2, 2025", description: "Subway", category: "Food", type: "Expense", amount: "$20.00" },
 
     ]
 
     return (
         <div className="bg-white p-4 rounded-lg shadow  lg:col-span-2 h-80">
             <h4 className="text-lg font-semibold ">Recent Transactions</h4>
-             <div className="overflow-x-auto">
-    <table className="min-w-full">
-      <thead>
-        <tr className="border-b border-gray-400 text-left text-sm tracking-wider">
+             <div className="max-h-64 overflow-y-auto">
+    <table className="min-w-full border-collapse">
+      <thead className="sticky top-0 border-b border-gray-400 text-left text-sm tracking-wider bg-gray-100">
+        <tr>
           <th className="py-3 px-4">Date</th>
           <th className="py-3 px-4">Description</th>
           <th className="py-3 px-4">Category</th>
@@ -32,7 +33,7 @@ function RecentTransactions () {
           <td className="py-3 px-4">{t.description}</td>
           <td className="py-3 px-4">{t.category}</td>
           <td className={`py-3 px-4 text-emerald-600 font-medium ${t.type === "Income" ? "text-emerald-600" : "text-red-500"}`}>{t.type}</td>
-          <td className={`py-3 px-4 text-right text-emerald-600 font-semibold ${t.type === "Income" ? "text-emerald-600" : "text-red-500"}`}>{t.amount}</td>
+          <td className={`py-3 px-4 text-right text-emerald-600 font-semibold ${t.type === "Income" ? "text-emerald-600" : "text-red-500"}`}>{t.type === "Income" ? "+" : "-"}{t.amount}</td>
         </tr>
        
     ))}
