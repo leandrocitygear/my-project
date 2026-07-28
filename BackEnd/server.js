e66af9c3e73667df641870374da3b65efe321383
Leandro De la cruz <leandrocitygear@icloud.com>
2026-07-28 13:18:35 -0400

    fixing env file

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,6 +2,9 @@ import express from 'express'
 import cors from 'cors'
 import router from './signupRouter.js'
 import loginRouter from './loginRouter.js';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
