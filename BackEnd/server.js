39af19d7357488e998da942c56b0a4837e977ce3
Leandro De la cruz <leandrocitygear@icloud.com>
2026-07-29 11:43:17 -0400

    new port

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -7,6 +7,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -14,7 +15,7 @@ app.use(express.json());
 app.use("/api", router);
 app.use("/api", loginRouter);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     console.log('Server running');
 });
 
